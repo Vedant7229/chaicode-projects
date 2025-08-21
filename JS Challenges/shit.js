55dd98dbf162d584046382e4a85d4c4cd3b5b0a2
Vedant7229 <vedantdalal7116@gmail.com>
2025-08-21 21:20:53 +0530

    shit.js

--- JS Challenges/shit.js
+++ JS Challenges/shit.js
@@ -15,16 +15,26 @@
 // filter
 // return - new array,
 // signature - filters out elements based on a condition
-arr = [1,2,3,4,5,6];
-Array.myFilter.prototype = function(useFn) {
-     const result1 = [];
- for(let i=0; i<this.length; i++){
-    if(useFn(this[i])) {
-        result1.push(this[i]);
- }
-}
- return result1;
+// arr = [1,2,3,4,5,6];
+// Array.myFilter.prototype = function(useFn) {
+//      const result1 = [];
+//  for(let i=0; i<this.length; i++){
+//     if(useFn(this[i])) {
+//         result1.push(this[i]);
+//  }
+// }
+//  return result1;
+// }
+
+//  arr.myFilter((e) => e>3);
+
+function checktruthyValue(value){
+    if(value){
+        console.log ("truthy")
+    }
+    else{
+        console.log("falsy")
+    }
 }
 
- arr.myFilter((e) => e>3);
-
+checktruthyValue([1,2])
